100ba471d2ab2d01be350c01c0bd3843380c7320
Benjamin BERNARD <benjamin.bernard@openpathview.fr>
2017-10-14 13:39:20 +0200

    Adding OpenSFM Ressources, Fixes #12

--- opv_api_client/exceptions.py
+++ opv_api_client/exceptions.py
@@ -5,5 +5,21 @@ class RequestAPIException(APIException):
     """Occur when there is an error in the request (e.g 404 or 400 or 500...)"""
     def __init__(self, *args, response, **kwargs):
         self.response = response
-        super().__init__(self, *args, **kwargs)
+        self.message = self.printable_request() + "\r\n" + self.printable_reponse()
+        super().__init__(self, self.message)
 
+    def printable_request(self):
+        """ Gives a fully printable version of the request """
+        req = self.response.request
+        msg = "-- Request : {} | {} -- \r\n".format(req.method, req.url)
+        msg += "Headers: {} \r\n".format(str(req.headers))
+        msg += "Body: {} \r\n\r\n".format(str(req.body))
+        return msg
+
+    def printable_reponse(self):
+        """ Gives a fully printable version of the response """
+        resp = self.response
+        msg = "-- Reponse : {} -- \r\n".format(resp.status_code)
+        msg += "Headers: {} \r\n".format(str(resp.headers))
+        msg += "Body: {} \r\n\r\n".format(str(resp.content))
+        return msg
