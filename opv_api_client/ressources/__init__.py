99717f07b78a89099819b88e2e21c09ddd6dd862
Christophe NOUCHET <nouchet.christophe@gmail.com>
2018-08-18 05:07:13 +0200

    Attempt to add path_details, ath_edge and path_node to ressources

--- opv_api_client/ressources/__init__.py
+++ opv_api_client/ressources/__init__.py
@@ -8,5 +8,11 @@ from opv_api_client.ressources.trackedge import TrackEdge
 from opv_api_client.ressources.reconstruction import Reconstruction
 from opv_api_client.ressources.shot import Shot
 from opv_api_client.ressources.path import Path
+from opv_api_client.ressources.path_node import PathNode
+from opv_api_client.ressources.path_edge import PathEdge
+from opv_api_client.ressources.path_details import PathDetails
 
-__all__ = ["Lot", "Campaign", "Cp", "Panorama", "Tile", "Sensors", "TrackEdge", "Reconstruction", "Shot", "Path"]
+__all__ = [
+    "Lot", "Campaign", "Cp", "Panorama", "Tile", "Sensors", "TrackEdge", "Reconstruction", "Shot", "Path", "PathNode",
+    "PathEdge", "PathDetails"
+]
---
+++ opv_api_client/ressources/path_details.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (C) 2017 Open Path View
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Contributors: Christophe NOUCHET
+# Email: christophe.nouchet@openpathview.fr
+# Description: Represent Path API ressource.
+
+from opv_api_client.relationship import Relationship
+from opv_api_client.ressource import Ressource
+from opv_api_client.ressource_list import register, RessourceProxy
+
+@register
+class PathDetails(Ressource):
+    _api_version = "v1"
+    _name = "path_details"
+    _primary_keys = ("id_path_details", "id_malette")
+
+    class _rel:
+        start_node = Relationship(RessourceProxy("path_node"))
+        stop_node = Relationship(RessourceProxy("path_node"))
+        campaign = Relationship(RessourceProxy("campaign"))
